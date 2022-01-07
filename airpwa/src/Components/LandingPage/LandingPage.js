3bce13d3066b11e51c5d162758546271c045e3f6
Priteshkamde <priteshkamde24@gmail.com>
2022-01-08 00:06:00 +0530

    Feature : Adding Stepper via Material UI

--- airpwa/src/Components/LandingPage/LandingPage.js
+++ airpwa/src/Components/LandingPage/LandingPage.js
@@ -21,7 +21,7 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
-
+import Stepss from '../Viz/Stepss';
 
 export default function Home() {
   const classes = useStyles();
@@ -125,6 +125,9 @@ export default function Home() {
               </Dialog>
             </Container>
           </main>
+          <hr />
+          <h1>Get Along</h1>
+          <Stepss />
         </Container>
         <br />
     </React.Fragment>
